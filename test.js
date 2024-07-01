5786e281efdc070257bbb2f597070c5d987c6569
Kanishk Vikram <kanishkvsingh@gmail.com>
2024-07-01 12:15:50 +0530

    DataType Conversion

--- test.js
+++ test.js
@@ -1,1 +1,17 @@
-console.log("Kanishk Vikram Singh")
+let score = null
+console.log(typeof score);
+
+score = Number(score)
+console.log(typeof score);
+
+console.log(score);
+
+// "33" => 33
+// "33abc" => NaN
+// null => 0
+// undefined => NaN
+// "kanishk" => NaN
+// true => 1 & false => 0
+
+
+
